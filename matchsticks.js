063d849c39a3e16315dd1f105bbfbd734d057e3b
Jamie Strachan <jamie.strachan@gmail.com>
2024-01-13 20:48:48 -0500

    finished basic system with fixed steps

--- matchsticks.js
+++ matchsticks.js
@@ -2,28 +2,44 @@ class Step {
     #proc = 0;
     #vel = 1;
     #prev = null;
+    #tc = 0;
 
     constructor(velocity, previousStep) { // fixed velocity
-        if (Number.isInteger(velocity)) {
+        if (velocity && Number.isInteger(velocity)) {
             this.#vel = velocity;
         }
 
         if (previousStep && Object.getPrototypeOf(previousStep) === Step.prototype) {
             this.#prev = previousStep;
         }
-
     }
 
     get processed() { return this.#proc; }
     get velocity() { return this.#vel; }
     get previousStep() { return this.#prev; }
+    get tickCount() { return this.#tc; }
 
-    tick() {
+    tick(count) {
+        let iterations = 1;
+        if (count && Number.isInteger(count) && count > 1) {
+            iterations = count;
+        }
+        
+        for (let i = 1; i <= iterations; i++) {
+            this.oneTick();
+        }
+    }
+
+    oneTick() {
         if (this.#prev) {
             this.#proc += this.#prev.pullForward(this.#vel);
         } else {
             this.#proc += this.#vel;
         }
+
+        this.#tc += 1;
+
+        return true;
     }
 
     pullForward(request) {
@@ -40,8 +56,83 @@ class Step {
         this.#proc -= available;
         return available;
     }
+
+    toString() {
+        let retval = "+" + this.#vel + " (" + this.#proc + ")";
+        return retval;
+    }
+}
+
+class System {
+    #steps = [];
+    #tc = 0;
+
+    get output() { 
+        if (this.#steps.length > 0) {
+            return this.#steps[this.#steps.length - 1].processed;
+        } else {
+            return 0;
+        }
+    }
+    get steps() { return this.#steps; }
+    get tickCount() { return this.#tc; }
+    get throughput() {
+        if (this.#tc === 0) {
+            return 0;
+        } else {
+            return this.output / this.#tc;
+        }
+    }
+
+    addStep(vel) {
+        let newStep;
+
+        if (this.#steps.length > 0) {
+            newStep = new Step(vel, this.#steps[this.#steps.length - 1]);
+        } else {
+            newStep = new Step(vel);
+        }
+
+        this.#steps.push(newStep);
+
+        return true;
+    }
+
+    tick(count) {
+        let iterations = 1;
+        if (count && Number.isInteger(count) && count > 1) {
+            iterations = count;
+        }
+        
+        for (let i = 1; i <= iterations; i++) {
+            this.oneTick();
+        }
+    }
+
+    oneTick() {
+        for (let i = this.#steps.length - 1; i >= 0; i--) {
+            this.#steps[i].tick();
+        }
+
+        this.#tc += 1;
+
+        return true;
+    }
+
+    toString() {
+        let retval = "";
+
+        for (let i = 0; i < this.#steps.length; i++ ) {
+            retval += "[" + this.#steps[i].toString() + "] ";
+        }
+
+        retval += "(" + this.output + ")";
+
+        return retval;
+    }
 }
 
 module.exports = {
     Step: Step,
+    System: System
 };
--- matchsticks.test.js
+++ matchsticks.test.js
@@ -1,4 +1,4 @@
-const { Step } = require('./matchsticks.js');
+const { Step, System } = require('./matchsticks.js');
 
 test('expect Step class to exist with defaults', () => {
     const step1 = new Step();
@@ -8,6 +8,7 @@ test('expect Step class to exist with defaults', () => {
     expect(step1.velocity).toBe(1);
     expect(step1.previousStep).toBe(null);
     expect(step1.tick).toBeDefined();
+    expect(step1.tickCount).toBe(0);
     
     const step2 = new Step(2);
     expect(step2.velocity).toBe(2);
@@ -17,6 +18,22 @@ test('expect Step class to exist with defaults', () => {
     
     const step4 = new Step(2.5); // bad initial value should force velocity to 1
     expect(step4.velocity).toBe(1);
+});
+
+test('Step.tickCount should increment with each tick()', () => {
+    const step1 = new Step();
+    expect(step1.tickCount).toBe(0);
+    
+    step1.tick();
+    expect(step1.tickCount).toBe(1);
+    step1.tick();
+    expect(step1.tickCount).toBe(2);
+    step1.tick();
+    expect(step1.tickCount).toBe(3);
+    step1.tick();
+    step1.tick();
+    step1.tick();
+    expect(step1.tickCount).toBe(6);
 });
 
 test('expect tick() to process items at velocity speed if there is no previous Step', () => {
@@ -41,6 +58,14 @@ test('expect tick() to process items at velocity speed if there is no previous S
     expect(step2.processed).toBe(10);
     step2.tick();
     expect(step2.processed).toBe(15);
+});
+
+test('pass an argument to Step.tick() to run multiple ticks', () => {
+    const step1 = new Step();
+
+    step1.tick(5);
+    expect(step1.tickCount).toBe(5);
+    expect(step1.processed).toBe(5);
 });
 
 test('expect pullForward() to reduce the available processed items', () => {
@@ -98,4 +123,141 @@ test('expect tick() to process items based on items available from the previous 
     step2.tick();
     expect(step2.processed).toBe(5);
     expect(step1.processed).toBe(0);
+});
+
+test('visualize Step', () => {
+    const step1 = new Step();
+
+    expect(step1.toString).toBeDefined();
+    expect(step1.toString()).toBe("+1 (0)");
+
+    step1.tick();
+    expect(step1.toString()).toBe("+1 (1)");
+
+    const step2 = new Step(4);
+    expect(step2.toString()).toBe("+4 (0)");
+
+    step2.tick();
+    step2.tick();
+    expect(step2.toString()).toBe("+4 (8)");
+})
+
+test('expect System class to exist with defaults', () => {
+    const sys1 = new System();
+
+    expect(sys1).toBeDefined();
+    expect(sys1.output).toBe(0);
+    expect(sys1.steps).toStrictEqual([]);
+    expect(sys1.addStep).toBeDefined();
+    expect(sys1.tick).toBeDefined();
+    expect(sys1.tickCount).toBe(0);
+});
+
+test('add Steps to a System', () => {
+    const sys1 = new System();
+
+    sys1.addStep();
+    sys1.addStep(3);
+    sys1.addStep(5);
+    
+    expect(sys1.steps[0].velocity).toBe(1);
+    expect(sys1.steps[1].velocity).toBe(3);
+    expect(sys1.steps[2].velocity).toBe(5);
+    expect(sys1.steps[1].previousStep).toStrictEqual(sys1.steps[0]);
+    expect(sys1.steps[2].previousStep).toStrictEqual(sys1.steps[1]);
+})
+
+test('tick through a System', () => {
+    const sys1 = new System();
+
+    sys1.addStep();
+    sys1.addStep();
+    sys1.addStep();
+
+    expect(sys1.steps[0].processed).toBe(0);
+    expect(sys1.steps[1].processed).toBe(0);
+    expect(sys1.steps[2].processed).toBe(0);
+    expect(sys1.output).toBe(0);
+    
+    sys1.tick();
+    expect(sys1.steps[0].processed).toBe(1);
+    expect(sys1.steps[1].processed).toBe(0);
+    expect(sys1.steps[2].processed).toBe(0);
+    expect(sys1.output).toBe(0);
+    
+    sys1.tick();
+    expect(sys1.steps[0].processed).toBe(1);
+    expect(sys1.steps[1].processed).toBe(1);
+    expect(sys1.steps[2].processed).toBe(0);
+    expect(sys1.output).toBe(0);
+    
+    sys1.tick();
+    expect(sys1.steps[0].processed).toBe(1);
+    expect(sys1.steps[1].processed).toBe(1);
+    expect(sys1.steps[2].processed).toBe(1);
+    expect(sys1.output).toBe(1);
+});
+
+test('System.tickCount should increment with each tick()', () => {
+    const sys1 = new System();
+    expect(sys1.tickCount).toBe(0);
+    
+    sys1.tick();
+    expect(sys1.tickCount).toBe(1);
+    sys1.tick();
+    expect(sys1.tickCount).toBe(2);
+    sys1.tick();
+    expect(sys1.tickCount).toBe(3);
+    sys1.tick();
+    sys1.tick();
+    sys1.tick();
+    expect(sys1.tickCount).toBe(6);
+});
+
+test('visualize System', () => {
+    const sys1 = new System();
+
+    expect(sys1.toString).toBeDefined();
+    
+    sys1.addStep(3);
+    sys1.addStep(2);
+    sys1.addStep(1);
+
+    sys1.tick();
+    expect(sys1.toString()).toBe("[+3 (3)] [+2 (0)] [+1 (0)] (0)");
+    
+    sys1.tick();
+    expect(sys1.toString()).toBe("[+3 (4)] [+2 (2)] [+1 (0)] (0)");
+
+    sys1.tick();
+    expect(sys1.toString()).toBe("[+3 (5)] [+2 (3)] [+1 (1)] (1)");
+
+    sys1.tick();
+    expect(sys1.toString()).toBe("[+3 (6)] [+2 (4)] [+1 (2)] (2)");
+});
+
+test('calculate System throughput', () => {
+    const sys1 = new System();
+
+    sys1.addStep(3);
+    sys1.addStep(2);
+    sys1.addStep(1);
+
+    sys1.tick();
+    expect(sys1.throughput).toBe(0);
+    sys1.tick();
+    expect(sys1.throughput).toBe(0);
+    sys1.tick();
+    expect(sys1.throughput).toBeCloseTo(0.33);
+    sys1.tick();
+    expect(sys1.throughput).toBe(0.5);
+});
+
+test('pass an argument to System.tick() to run multiple ticks', () => {
+    const sys1 = new System();
+
+    sys1.tick(5);
+    expect(sys1.tickCount).toBe(5);
+    expect(sys1.output).toBe(0);
+    expect(sys1.throughput).toBe(0);
 });
